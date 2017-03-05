39d5a191d7c6527656c8fcd6bdf8ac7499ca9191
daniel <danieljosebecerra@gmail.com>
2017-03-05 01:09:44 -0500

    fetching data and showing the json

--- index.android.js
+++ index.android.js
@@ -12,31 +12,71 @@ import {
   TextInput,
   Image,
   ListView,
+  TouchableHighlight,
   View
 } from 'react-native';
 
 
+
 export default class AwesomeProject extends Component {
    // Initialize the hardcoded data
+async getMoviesFromApi() {
+    try {
+      let response = await fetch('https://facebook.github.io/react-native/movies.json');
+      let responseJson = await response.json();
+      this.setState({
+        texto: responseJson.movies.map((data)=>(<Text>{data.title}</Text>))
+      })
+
+      return;
+    } catch(error) {
+      console.error('There is an error: ' + error);
+    }
+  }
+ 
+  
   constructor(props) {
     super(props);
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    
     this.state = {
-      dataSource: ds.cloneWithRows([
-        'John', 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
-      ])
-    };
+      dataSource: ds,
+      texto: null
   }
+}
+
+componentWillMount() {
+  this.getMoviesFromApi();
+}
+
+
+
   render() {
     return (
-      <View style={{flex: 1, paddingTop: 22}}>
-        <ListView
-          dataSource={this.state.dataSource}
-          renderRow={(rowData) => <Text>{rowData}</Text>}
-        />
-      </View>
+      <View style={styles.container}>
+                
+                <TouchableHighlight  style={styles.button}>
+                    <Text>{this.state.texto}</Text>
+                </TouchableHighlight>
+            </View>
     );
   }
 }
+
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    button: {
+        backgroundColor: '#eeeeee',
+        padding: 10,
+        marginRight: 5,
+        marginLeft: 5,
+    }
+});
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
