d37cc1b346899dee9761da699df85f6b7f41c90e
daniel <danieljosebecerra@gmail.com>
2017-04-08 12:29:11 -0400

    testing animations

--- index.android.js
+++ index.android.js
@@ -7,32 +7,20 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  TextInput,
-  Image,
-  ListView,
-  TouchableHighlight,
-  View
+  Text
 } from 'react-native';
-
-
+import { FadeInView as FadeInView }  from './archivos/FadeInView.js';
 
 export default class AwesomeProject extends Component {
    // Initialize the hardcoded data
- _onPressButton() {
-    console.log("You tapped the button!");
-  }
-
   render() {
     return (
-      <TouchableHighlight onPress={this._onPressButton}>
-        <Text>Button</Text>
-      </TouchableHighlight>
-    );
+      <FadeInView style={{width: 100, height: 50, backgroundColor: 'powderblue'}}>
+        <Text style={{fontSize: 10, textAlign: 'center', margin: 10}}>Fading in</Text>
+      </FadeInView>
+    )
   }
 }
-
 
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
