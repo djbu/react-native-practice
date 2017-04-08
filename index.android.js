3b98451c604013fb962ac057fddcdeec56e5f034
daniel <danieljosebecerra@gmail.com>
2017-04-08 02:22:47 -0400

    testing tappable components

--- index.android.js
+++ index.android.js
@@ -20,63 +20,19 @@ import {
 
 export default class AwesomeProject extends Component {
    // Initialize the hardcoded data
-async getMoviesFromApi() {
-    try {
-      let response = await fetch('https://facebook.github.io/react-native/movies.json');
-      let responseJson = await response.json();
-      this.setState({
-        texto: responseJson.movies.map((data)=>(<Text>{data.title}</Text>))
-      })
-
-      return;
-    } catch(error) {
-      console.error('There is an error: ' + error);
-    }
+ _onPressButton() {
+    console.log("You tapped the button!");
   }
- 
-  
-  constructor(props) {
-    super(props);
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    
-    this.state = {
-      dataSource: ds,
-      texto: null
-  }
-}
-
-componentWillMount() {
-  this.getMoviesFromApi();
-}
-
-
 
   render() {
     return (
-      <View style={styles.container}>
-                
-                
-                <Image source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}
-       style={{width: 400, height: 400}} />
-            </View>
+      <TouchableHighlight onPress={this._onPressButton}>
+        <Text>Button</Text>
+      </TouchableHighlight>
     );
   }
 }
 
-var styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    button: {
-        backgroundColor: '#eeeeee',
-        padding: 10,
-        marginRight: 5,
-        marginLeft: 5,
-    }
-});
+
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
